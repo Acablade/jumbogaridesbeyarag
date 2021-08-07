4d8d353014378f9bae1f5de65865537b7af9206a
Acablade <ogzylmzlr26@yahoo.com>
2021-08-07 15:44:39 +0300

    Fixed path issues

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/',(req,res) => {
     res.set({
         'Refresh': '5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     })
-    res.sendFile(path.join(__dirname +'/index.html'))
+    res.sendFile(path.join(__dirname,'index.html'))
 })
 
 app.listen(3000)
