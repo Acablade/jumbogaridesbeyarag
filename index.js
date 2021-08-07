ddfd82dc65c76dd3970c1ed73e87784e5ce14004
Acablade <ogzylmzlr26@yahoo.com>
2021-08-08 00:07:18 +0300

    Added new videos

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 
-const data = {
-    videos:[
+const data = [
         "S0yLbdZRu3A",
         "_S7WEVLbQ-Y",
         "DN0gAQQ7FAQ",
@@ -22,9 +21,60 @@ const data = {
         "yf-SKbbQJHY",
         "8lTY6LWGoXY",
         "Jz5udRnFVTQ",
-        "vcsvN3ADrRs"
+        "vcsvN3ADrRs",
+        "Vu94KGpV4Pg",
+        "QZShA_a-5r8",
+        "MLzK8R6XqUk",
+        "L_jWHffIx5E",
+        "afpNaQyHaDw",
+        "0imhOmhRgfI",
+        "246cTmV3vr8",
+        "kGpBmrbtctY",
+        "sHeHGeNYkRs",
+        "JUJ_cr11PSY",
+        "6M-NkQAo-3E",
+        "hH9M-m3WD0g",
+        "BJk6gZuPKRE",
+        "tRWpY3TV-j4",
+        "W8x4m-qpmJ8",
+        "VN-DG5MgDy0",
+        "UXiwRmlCZ7E",
+        "g3jCAyPai2Y",
+        "waKumDkYrDY",
+        "M5V_IXMewl4",
+        "KF32DRg9opA",
+        "G70S5fumHso",
+        "z97qLNXeAMQ",
+        "1tilu4vEh0k",
+        "kLp_Hh6DKWc",
+        "z5LW07FTJbI",
+        "fPO76Jlnz6c",
+        "SFzMs2SN--s",
+        "E5DCW-7-hCQ",
+        "4IXOM4Q2Apc",
+        "oJlfuBHLf0o",
+        "GCspMiRl2C0",
+        "pcHjO3C-3TQ",
+        "y-J4fJ85ARs",
+        "7Kn0V5mpJe4",
+        "YTeUdtilREo",
+        "xe2FPP4lX14",
+        "g2x2dEerjQo",
+        "4GmLoVGmgDw",
+        "Wjar_TtxnV4",
+        "K3Qzzggn--s",
+        "AuEjB_AO90Y",
+        "2MtOpB5LlUA",
+        "AOeY-nDp7hI",
+        "6l6vqPUM_FE",
+        "IIrCDAV3EgI",
+        "Cfw85wT13Bg",
+        "iX6ex5fYT7o",
+        "GlUeW7IOSFc",
+        "TwVBfMoSAu4",
+        "2iNjot3oCxA",
+        "3I8_Q5ftBpA"
     ]
-}
 
 app.set('view engine', 'ejs');
 
@@ -39,7 +89,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req,res) => {
 
-    var item = data.videos[Math.floor(Math.random()*data.videos.length)];
+    var item = data[Math.floor(Math.random()*data.length)];
     res.render("index.ejs",{
         url:item
     })
