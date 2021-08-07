0961e4fe6fc9fe00d0ceacc6e4f7cd458cd4dd1e
Acablade <ogzylmzlr26@yahoo.com>
2021-08-07 18:10:02 +0300

    Fixed some bugs

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const path = require('path')
 const app = express()
 
 app.use((req,res,next) => {
-    res.header('Refresh','5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    //res.header('Refresh','5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ')
     next()
 })
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 
 
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'index.html')) 
+    res.sendFile('index.html')
     /*res.set({
         'Access-Control-Expose-Headers': 'ETag',
         'Refresh': '5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
