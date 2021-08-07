6917456260e4114368f77acffcef3b405482f3af
Acablade <ogzylmzlr26@yahoo.com>
2021-08-07 17:30:12 +0300

    Changed video from Hotel Room Service to Polish toilet spin

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use((req,res,next) => {
+    res.header('Refresh','5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    next()
+})
+
 app.use(express.static(__dirname));
 
+
+
 app.get('/',(req,res) => {
-    
-    res.set({
-        'Refresh': '5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-    })
     res.sendFile(path.join(__dirname,'index.html')) 
+    /*res.set({
+        'Access-Control-Expose-Headers': 'ETag',
+        'Refresh': '5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    })*/
 })
 
 app.listen(process.env.PORT || 3000, function(){
