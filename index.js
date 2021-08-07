bf4fbc9a1d04a792301ce65718dff7d9641d2e66
Acablade <ogzylmzlr26@yahoo.com>
2021-08-07 17:06:29 +0300

    Port listen fix

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.get('/',(req,res) => {
     res.set({
         'Refresh': '5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     })
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.sendFile(path.join(__dirname,'index.html')) 
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
